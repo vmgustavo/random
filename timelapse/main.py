17845ccec86e22a79e58484311acf9c22d5418d0
Gustavo Maia <gustavo.maia@cinnecta.com>
2021-09-24 19:37:49 -0300

    add click options

--- timelapse/main.py
+++ timelapse/main.py
@@ -1,27 +1,43 @@
+import os
+import click
 from glob import glob
 
 import cv2
 from tqdm import tqdm
 import moviepy.editor as moviepy
 
-files = glob('data/raw/*.jpg')
-height, width, layers = cv2.imread(files[0]).shape
-size = (width, height)
 
-arr = [cv2.imread(file) for file in sorted(files)]
+@click.command()
+@click.option('--fps', default=10)
+def main(fps):
+    files = glob('data/raw/*.jpg')
+    height, width, layers = cv2.imread(files[0]).shape
+    size = (width, height)
 
-out = cv2.VideoWriter(
-    filename='data/processed/timelapse.avi',
-    fourcc=cv2.VideoWriter_fourcc(*'DIVX'),
-    fps=10, frameSize=size
-)
-for elem in tqdm(arr, desc='Building video'):
-    out.write(elem)
-out.release()
+    arr = [cv2.imread(file) for file in sorted(files)]
 
-clip = moviepy.VideoFileClip('data/processed/timelapse.avi')
-clip.write_videofile(
-    filename='data/processed/timelapse.mp4',
-    codec='mpeg4'
-)
-clip.close()
+    avi = f'data/processed/timelapse-fps{fps}.avi'
+    mp4 = f'data/processed/timelapse-fps{fps}.mp4'
+
+    if os.path.exists(avi):
+        print('Skip AVI')
+    else:
+        out = cv2.VideoWriter(
+            filename=avi,
+            fourcc=cv2.VideoWriter_fourcc(*'DIVX'),
+            fps=fps, frameSize=size
+        )
+        for elem in tqdm(arr, desc='Building video'):
+            out.write(elem)
+        out.release()
+
+    if os.path.exists(mp4):
+        print('Skip MP4')
+    else:
+        clip = moviepy.VideoFileClip(avi)
+        clip.write_videofile(filename=mp4, codec='mpeg4')
+        clip.close()
+
+
+if __name__ == '__main__':
+    main()
